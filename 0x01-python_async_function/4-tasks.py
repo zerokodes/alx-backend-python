2f47d770541b3de2500e16163d322d82df5f1592
Nduka Henry Chisom <henrysam140@gmail.com>
2023-07-11 03:59:29 +0100

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,31 +1,27 @@
 #!/usr/bin/env python3
+""" defines a routine wait_n that takes in 2
+    int arguments (in this order): n and max_delay
+"""
+import typing
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-from typing import List
-
-
-''' Take the code from wait_n and alter it into a new function task_wait_n.
-    The code is nearly identical to wait_n except task_wait_random is being
-    called.
-'''
-
-
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''Runs an async function for n times and adds the results into a list'''
-    modified_random = __import__('3-tasks').task_wait_random
-
-    delay_list = []
-    i = 0
-
-    while i < n:
-        delay_list.append(await modified_random(max_delay))
-        i += 1
-
-    return sorted(delay_list)
-
-
-if __name__ == '__main__':
-    import asyncio
-
-    print(task_wait_n.__doc__)
-    print(asyncio.run(task_wait_n(3, 4)))
+async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """ Spawns wait_random n times with specified max_delay
+        return: A list of all delays(floats) in ascending order.
+    """
+    res_list = []
+    sorted_list = []
+    for i in range(n):
+        res = await task_wait_random(max_delay)
+        res_list.append(res)
+    # Sort the list in ascending order.
+    while res_list:
+        MIN = res_list[0]
+        for x in res_list:
+            if x < MIN:
+                MIN = x
+        sorted_list.append(MIN)
+        res_list.remove(MIN)
+    return sorted_list
